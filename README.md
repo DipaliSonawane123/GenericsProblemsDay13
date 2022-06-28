b9930511d4429b29043e37d976bf2eb68e868c3c
Dipali <dipssonawane123@gmail.com>
2022-06-28 20:51:37 +0530

    Added:find maximun no from three no program is added

---
+++ README.md
@@ -0,0 +1,1 @@
+# GenericsProblemsDay13
---
+++ src/com/javagenerics/FindTheMaxInteger.java
@@ -0,0 +1,8 @@
+package com.javagenerics;
+
+public class FindTheMaxInteger {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Find Maximum Number From Three Integer program");
+    }
+}
+
